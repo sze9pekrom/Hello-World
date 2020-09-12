bddde5e6ceceb94ad4758f1185bccb772cf66f6f
pekarszkijroman@protonmail.com <pekarszkijroman@protonmail.com>
2020-09-12 14:06:49 +0200

    init

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+	public static void main(String[] args) { //szöveges tömb
+		/*
+		System.out.println("Hello World"); //syso ctrl+tab
+		System.out.println(Integer.parseInt(args[2]) + Integer.parseInt(args[1]));
+		string 
+		*/
+		System.out.println("Vezeteknev: "+ args[0]);
+		System.out.println("Keresztnev: "+ args[1]);
+		System.out.println("Kor: "+ args[2]);
+	}
+}
